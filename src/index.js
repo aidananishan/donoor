9a4135c21028441fb1e8d4e8f5b098d3bd0a108e
AidanaN <aidana.nishanbaeva.kk@gmail.com>
2022-06-02 12:54:28 +0600

    Fix bad repo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App';
+import './modal';
+import './Accordion';
 import App from './App'; 
-// import './behero';
-// import './behero.css';
-// import './find';
-// import './find.css'
-
-import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,10 +11,6 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+); 
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
